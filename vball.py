60edc6223e706f628cbb71dc0f65193e0a963643
CoreyFulop <corey.fulop@gmail.com>
2022-07-21 15:52:03 +0100

    Exercise 3

---
+++ vball.py
@@ -0,0 +1,50 @@
+# vball.py
+
+from random import random
+
+def printIntro():
+    print("This program simulates a game of volleyball")
+    print("between teams 'A' and 'B'. The abilities of")
+    print("each team are indicated by a probability (a")
+    print("number between 0 and 1) that the team will")
+    print("win a point when serving. The game starts with")
+    print("team A serving.")
+
+def getInputs():
+    probA = eval(input("What is the prob. team A wins a serve? "))
+    probB = eval(input("What is the prob. team B wins a serve? "))
+    return probA, probB
+
+def simGame(probA, probB):
+    pointsA = pointsB = 0
+    server = "A"
+    while not endGame(pointsA, pointsB):
+        if server == "A":
+            if random() < probA:
+                pointsA += 1
+            else:
+                server = "B"
+        else:
+            if random() < probB:
+                pointsB += 1
+            else:
+                server = "A"
+    return pointsA, pointsB
+
+def endGame(pointsA, pointsB):
+    return (pointsA >= 15 and pointsA >= pointsB + 2) or (pointsB >= 15 and pointsB >= pointsA + 2)
+
+def results(pointsA, pointsB):
+    if pointsA > pointsB:
+        print(f"Team A wins, {pointsA} to {pointsB}.")
+    else:
+        print(f"Team B wins, {pointsB} to {pointsA}.")
+
+def main():
+    printIntro()
+    probA, probB = getInputs()
+    pointsA, pointsB = simGame(probA, probB)
+    results(pointsA, pointsB)
+
+if __name__ == "__main__":
+    main()
